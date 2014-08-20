023a9ebebfe3d68e92b64669197eb28d83441b78
amosli <amosli@juxinli.com>
2014-08-20 23:53:39 +0800

    图片查看器

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,84 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pboGirls = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbTime = new System.Windows.Forms.Label();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pboGirls)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pboGirls
+            // 
+            this.pboGirls.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pboGirls.Image = ((System.Drawing.Image)(resources.GetObject("pboGirls.Image")));
+            this.pboGirls.Location = new System.Drawing.Point(0, 0);
+            this.pboGirls.Name = "pboGirls";
+            this.pboGirls.Size = new System.Drawing.Size(876, 452);
+            this.pboGirls.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pboGirls.TabIndex = 1;
+            this.pboGirls.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbTime
+            // 
+            this.lbTime.AutoSize = true;
+            this.lbTime.Location = new System.Drawing.Point(43, 13);
+            this.lbTime.Name = "lbTime";
+            this.lbTime.Size = new System.Drawing.Size(0, 12);
+            this.lbTime.TabIndex = 2;
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(148, 405);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(75, 23);
+            this.btnPrevious.TabIndex = 3;
+            this.btnPrevious.Text = "上一页";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(589, 405);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 4;
+            this.btnNext.Text = "下一页";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 452);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.lbTime);
+            this.Controls.Add(this.pboGirls);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.timer1_Tick);
+            ((System.ComponentModel.ISupportInitialize)(this.pboGirls)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pboGirls;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbTime;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,35 @@ namespace PictureBox控件学习
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lbTime.Text = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+        public static String ImgDirPath = @"C:\work\img";
+        String[] imgPaths = Directory.GetFiles(ImgDirPath, "*.jpg");
+        public static int ImgNum = 0;
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            ImgNum--;
+            if (ImgNum < 0)
+            {
+                ImgNum = imgPaths.Length-1;
+            }
+            pboGirls.ImageLocation = imgPaths[ImgNum];
+
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            ImgNum++;
+            if (ImgNum >= imgPaths.Length)
+            {
+                ImgNum = 0;
+            }
+            pboGirls.ImageLocation = imgPaths[ImgNum];
         }
     }
 }
