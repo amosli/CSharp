bf58472b92c39814154a82fee645e344a5822d5d
amosli <amosli@juxinli.com>
2014-08-21 01:48:42 +0800

    mdi study

---
+++ mdi设计/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mdi设计
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 显示子窗体ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form f2 = new Form2();
+            f2.MdiParent = this;//指定其mdi父窗体
+            f2.Show();
+            
+            
+            Form f3 = new Form3();
+            f3.MdiParent = this;
+            f3.Show();
+
+            Form f4 = new Form4();
+            f4.Show();
+            f4.MdiParent = this;
+
+
+        }
+
+        private void 横向排列ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void 纵向排列ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void 关闭全部窗体ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (Form f in this.MdiChildren)
+            {
+                f.Close();
+            }
+
+        }
+    }
+}
