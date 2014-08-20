ce59e52558b53a73b4a1a9a28dc36e0a54cf6d56
amosli <amosli@juxinli.com>
2014-08-21 01:56:46 +0800

    load bestpay widget by webbrowser

---
+++ LoadWidget/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace LoadWidget
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.复制ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.粘贴ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.剪切ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.清空ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.GO = new System.Windows.Forms.Button();
+            this.urlInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.refresh = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.复制ToolStripMenuItem,
+            this.粘贴ToolStripMenuItem,
+            this.剪切ToolStripMenuItem,
+            this.清空ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 92);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // 复制ToolStripMenuItem
+            // 
+            this.复制ToolStripMenuItem.Name = "复制ToolStripMenuItem";
+            this.复制ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.复制ToolStripMenuItem.Text = "复制";
+            // 
+            // 粘贴ToolStripMenuItem
+            // 
+            this.粘贴ToolStripMenuItem.Name = "粘贴ToolStripMenuItem";
+            this.粘贴ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.粘贴ToolStripMenuItem.Text = "粘贴";
+            // 
+            // 剪切ToolStripMenuItem
+            // 
+            this.剪切ToolStripMenuItem.Name = "剪切ToolStripMenuItem";
+            this.剪切ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.剪切ToolStripMenuItem.Text = "剪切";
+            // 
+            // 清空ToolStripMenuItem
+            // 
+            this.清空ToolStripMenuItem.Name = "清空ToolStripMenuItem";
+            this.清空ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.清空ToolStripMenuItem.Text = "清空";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(12, 71);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(882, 345);
+            this.webBrowser1.TabIndex = 4;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // GO
+            // 
+            this.GO.Location = new System.Drawing.Point(536, 24);
+            this.GO.Name = "GO";
+            this.GO.Size = new System.Drawing.Size(75, 23);
+            this.GO.TabIndex = 5;
+            this.GO.Text = "GO";
+            this.GO.UseVisualStyleBackColor = true;
+            this.GO.Click += new System.EventHandler(this.GO_Click);
+            // 
+            // urlInput
+            // 
+            this.urlInput.Location = new System.Drawing.Point(245, 26);
+            this.urlInput.Name = "urlInput";
+            this.urlInput.Size = new System.Drawing.Size(266, 21);
+            this.urlInput.TabIndex = 6;
+            this.urlInput.TextChanged += new System.EventHandler(this.urlInput_TextChanged);
+            this.urlInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.urlInput_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(34, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 23);
+            this.button1.TabIndex = 7;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(93, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(32, 23);
+            this.button2.TabIndex = 8;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // refresh
+            // 
+            this.refresh.Image = ((System.Drawing.Image)(resources.GetObject("refresh.Image")));
+            this.refresh.Location = new System.Drawing.Point(149, 26);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(30, 25);
+            this.refresh.TabIndex = 9;
+            this.refresh.UseVisualStyleBackColor = true;
+            this.refresh.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(650, 24);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 416);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.refresh);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.urlInput);
+            this.Controls.Add(this.GO);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 复制ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 粘贴ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 剪切ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 清空ToolStripMenuItem;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button GO;
+        private System.Windows.Forms.TextBox urlInput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button refresh;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ LoadWidget/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoadWidget
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 禁止新弹出的窗口，好像没有什么作用
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        void webBrowser1_NewWindow(object sender, CancelEventArgs e)
+        {
+            //this.webBrowser1.Document.InvokeScript("function aa(){window.alert=null;window.confirm=null;window.open=null;window.showModalDialog=null;window.close=null}");
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            //js容错
+            webBrowser1.ScriptErrorsSuppressed = true;
+            //禁止新弹出的窗口
+            this.webBrowser1.NewWindow += new CancelEventHandler(webBrowser1_NewWindow);
+
+        }
+        public void CreateFile(String fileName,String html)
+        {
+            try
+            {
+                String dir = "C://work//";
+                // 创建文件
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+                FileStream fs = new FileStream(dir + fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                StreamWriter sw = new StreamWriter(fs); // 创建写入流
+                sw.WriteLine(html); // 写入Hello World
+                sw.Close(); //关闭文件
+            }
+            catch (Exception ex)
+            {
+                CreateFile("exception" + DateTime.Now.ToString("yyyy-MM-dd HH.mm.ss") + ".html", ex.ToString());
+            }
+           
+        }
+
+       
+
+        private void GO_Click(object sender, EventArgs e)
+        {
+             String url = urlInput.Text.Trim();
+            if (String.IsNullOrEmpty(url))
+            {
+                url = "http://www.baidu.com";
+            }
+            webBrowser1.Navigate(url);
+            webBrowser1.Navigate(@"C:\Users\amosli\Desktop\ocx.html");
+            url = "https://www.bestpay.com.cn/api/captcha/getCode";
+            httpClient.GetStringAsync(new Uri(url));
+            
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void urlInput_Enter(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(urlInput.Text);
+        }
+
+        private void urlInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                MessageBox.Show("即将访问：" + urlInput.Text);
+                webBrowser1.Navigate(urlInput.Text);
+            }
+        }
+
+        private void urlInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            LoginPay();
+        }
+
+
+        public HttpClient httpClient = new HttpClient();
+        public String productNo = "手机号码";
+        public String password = "vjHmAw8T7DOw70NmGFvitQ==";
+        public String url = "https://www.bestpay.com.cn/index";
+
+
+        public void LoginPay()
+        {
+            //while (webBrowser1.Document.GetElementById("encrypt") == null)
+            //{
+            //    System.Threading.Thread.Sleep(1000);
+            //}
+
+            //String password = webBrowser1.Document.GetElementById("encrypt").InnerText;
+
+            productNo = "手机号码";
+            password = "vjHmAw8T7DOw70NmGFvitQ==";
+            url = "https://www.bestpay.com.cn/index";
+
+            // Always catch network exceptions for async methods
+            try
+            {
+                MessageBox.Show("url:" + url);
+                var result = httpClient.GetStringAsync(new Uri(url)).Result;
+                CreateFile(DateTime.Now.ToString("yyyy-MM-dd HH.mm.ss") + ".html", result.ToString());
+
+                //bpsid:b620b3c8Z93712d4fZ147e2322e3aZ2098
+
+
+                String imgCode = "";
+                url = "https://www.bestpay.com.cn/api/captcha/getCode";
+                result = httpClient.GetStringAsync(new Uri(url)).Result;
+                CreateFile(DateTime.Now.ToString("yyyy-MM-dd HH.mm.ss") + ".png", result.ToString());
+
+                url = "https://www.bestpay.com.cn/api/captcha/match?actualToken=" + imgCode;
+                result = httpClient.GetStringAsync(new Uri(url)).Result;
+                CreateFile(DateTime.Now.ToString("yyyy-MM-dd HH.mm.ss") + ".png", result.ToString());
+
+                url = "https://www.bestpay.com.cn/api/security/getRandomString?key=keyPassword";
+                result = httpClient.GetStringAsync(new Uri(url)).Result;
+                CreateFile(DateTime.Now.ToString("yyyy-MM-dd HH.mm.ss") + ".html", result.ToString());
+                //z0ucvvpzjxzvcyvmvv8jl5kxukz08moc
+
+                // /UIlLgT+92BN90eygkOiDg==
+                // Bn4IocfkSRlZrbxKYB0eUg==
+                // Kv8ARrvDPL8y8cVttv39Rw==  
+
+
+                // vjHmAw8T7DOw70NmGFvitQ==
+
+                 //vjHmAw8T7DOw70NmGFvitQ==
+
+                
+
+                //productNo=手机号码&vcode=dxy7&password=vjHmAw8T7DOw70NmGFvitQ==&target=
+                url = "https://www.bestpay.com.cn/api/user/otherLogin";
+                var pairs = new List<KeyValuePair<String, String>>();
+
+                password = Regex.Replace(password, "=", "%3D");
+                pairs.Add(new KeyValuePair<string, string>("productNo", productNo));
+                pairs.Add(new KeyValuePair<string, string>("vcode", imgCode));
+                pairs.Add(new KeyValuePair<string, string>("password", password));
+                pairs.Add(new KeyValuePair<string, string>("target", ""));
+                var content = new FormUrlEncodedContent(pairs);
+
+                //result = httpClient.PostAsync(url, content).Result;
+                string resultContent = httpClient.PostAsync(url, content).Result.Content.ReadAsStringAsync().Result;
+                CreateFile(DateTime.Now.ToString("yyyy-MM-dd HH.mm.ss") + ".html", result.ToString());
+
+                url = "https://www.bestpay.com.cn/mypay/home";
+                result = httpClient.GetStringAsync(new Uri(url)).Result;
+            }
+            catch (Exception ex)
+            {
+                CreateFile("exception" + DateTime.Now.ToString("yyyy-MM-dd HH.mm.ss") + ".html", ex.ToString());
+            }
+
+            // Once your app is done using the HttpClient object call dispose to 
+            // free up system resources (the underlying socket and memory used for the object)
+            httpClient.Dispose();
+        }
+    }
+}
